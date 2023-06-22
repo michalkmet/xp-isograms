fa9dbd7fa55fd6fc2f85353b57497b916fa386ae
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2023-06-22 09:15:17 +0200

    4th test pass

--- src/isograms.test.js
+++ src/isograms.test.js
@@ -10,4 +10,7 @@ describe('isIsogram', () => {
   it('should return false when given aba', () => {
     expect(isIsogram('aba')).toBe(false);
   });
+  it('should return true when given a', () => {
+    expect(isIsogram('a')).toBe(true);
+  });
 });
